8a1c56982596e40ebbf858e02e3c94e8e3f4d965
Chris Young and Oskar Radon <ctopheryoung@gmail.com>
2016-01-20 13:49:05 -0800

    add passing spec for first two letters qu

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 var pigLatin = function(input) {
   if (input[0] === 'a' || input[0] === 'e' || input[0] === 'i' || input[0] === 'o' || input[0] === 'u') {
     return input + "ay"; //if input starts with vowel
-    
+
+  } else if (input[0] === 'q' && input[1] === 'u'){
+    return input.slice(2) + input[0] + input[1] + ("ay");//if first two letters are qu
+
   } else if ((input[0] != 'a' || input[0] != 'e' || input[0] != 'i' || input[0] != 'o' || input[0] != 'u')
   && (input[1] === 'a' || input[1] === 'e' || input[1] === 'i' || input[1] === 'o' || input[1] === 'u')) {
     return input.slice(1) + input[0] + ("ay"); //if first letter is a consonant, second letter is vowel
--- spec/specs.js
+++ spec/specs.js
@@ -8,10 +8,14 @@ describe("pigLatin", function() {
   });
 
   it ("takes two consecutive consonants at the begining of a word, moves it to the end and adds ay", function () {
-    expect(pigLatin("twenty")).to.equal("entytway")
+    expect(pigLatin("twenty")).to.equal("entytway");
   });
 
   it ("takes three consecutive consonants at the begining of a word, moves it to the end and adds ay", function(){
-    expect(pigLatin("three")).to.equal("eethray")
+    expect(pigLatin("three")).to.equal("eethray");
+  });
+
+  it ("takes first two letters when they are 'qu', moves them to the end and add 'ay'", function(){
+    expect(pigLatin("question")).to.equal("estionquay");
   });
 });
