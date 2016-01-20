04cc2275f10ca2c623ad071a12d1bbbfe87d8f97
Chris Young and Oskar Radon <ctopheryoung@gmail.com>
2016-01-20 10:31:19 -0800

    Add second spec for words that start with vowel

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
 var pigLatin = function(input) {
-  var output = input.concat("ay");
-  return output;
+  if (input[0] === 'a' || input[0] === 'e' || input[0] === 'i' || input[0] === 'o' || input[0] === 'u') {
+    return input + "ay";
+  } else {
+    return input.slice(1) + input[0] + ("ay");
+  }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -3,4 +3,8 @@ describe("pigLatin", function() {
     expect(pigLatin("apple")).to.equal("appleay");
   });
 
+  it("takes a single consonant at the beginning of a word and moves it to the end", function () {
+    expect(pigLatin("dog")).to.equal("ogday");
+  });
+
 });
